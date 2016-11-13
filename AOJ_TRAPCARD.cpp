bab38b5a10f477c48ad02e987514c363fc2a01ff
Won Seok Lee <wonseokdjango@ip-172-31-27-211.ap-northeast-2.compute.internal>
2016-11-13 10:54:08 +0000

    TRAPCARD is revised.

--- AOJ_TRAPCARD.cpp
+++ AOJ_TRAPCARD.cpp
@@ -1,38 +1,37 @@
-#include <queue>
-#include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
+#include <queue>
+#include <list>
 
 #define CANUYES_DBG
 #define MAXH 20
 #define MAXW 20
 
+using namespace std;
+
 const int dh[4] = {-1, 0, 1, 0};
 const int dw[4] = {0, 1, 0, -1};
 
-using namespace std;
-
-int H, W, T;
+int  H, W;
 char CAMP[MAXH][MAXW];
-
-int CAPA[MAXH * MAXW + 2][MAXH * MAXW + 2];
-int FLOW[MAXH * MAXW + 2][MAXH * MAXW + 2];
+int  CAPA[MAXH * MAXW + 2][MAXH * MAXW + 2];
+int  FLOW[MAXH * MAXW + 2][MAXH * MAXW + 2];
 
 bool safe (int i, int j) {
-
+    
     return (0 <= i && i < H && 0 <= j && j < W);
 }
 
 int mcmf (void) {
 
-    int ret = 0;
     int src = 0, sink = 1;
-    int prev[MAXH * MAXW + 2];
+    int ret = 0, prev[MAXH * MAXW + 2];
 
     while (true) {
 
         queue<int> q;
-        fill(prev, prev + MAXH * MAXW + 2, -1);
+        fill(prev, prev + (MAXH * MAXW + 2), -1);
 
         q.push(src);
         prev[src] = -2;
@@ -41,9 +40,7 @@ int mcmf (void) {
             q.pop();
 
             for (int i = 0; i < H * W + 2; ++i) {
-                if (prev[i] == -1 &&
-                    CAPA[here][i] - FLOW[here][i] > 0) {
-                    
+                if (prev[i] == -1 && CAPA[here][i] - FLOW[here][i] > 0) {
                     prev[i] = here;
                     q.push(i);
                 }
@@ -53,112 +50,119 @@ int mcmf (void) {
         if (prev[sink] == -1)
             break;
 
-        int cur = sink;
-        int minFlow = H * W + 2;
-        while (prev[cur] != -2) {
-            minFlow = min(minFlow, CAPA[prev[cur]][cur] - FLOW[prev[cur]][cur]);
-            cur = prev[cur];
+        int here = sink;
+        int flow = MAXH * MAXW + 2;
+
+        while (prev[here] != -2) {
+            flow = min(flow, CAPA[prev[here]][here] - FLOW[prev[here]][here]);
+            here = prev[here];
         }
 
-        ret += minFlow;
+        ret += flow;
+        here = sink;
 
-        cur = sink;
-        while (prev[cur] != -2) {
-            FLOW[prev[cur]][cur] += minFlow;
-            FLOW[cur][prev[cur]] -= minFlow;
-            cur = prev[cur];
+        while (prev[here] != -2) {
+            FLOW[prev[here]][here] += flow;
+            FLOW[here][prev[here]] -= flow;
+            here = prev[here];
         }
     }
 
     return ret;
 }
 
-void install (void) {
+unsigned int install (void) {
 
-    int pairs[MAXH * MAXW];
+    int  pairs[MAXH * MAXW];
+    bool hasPairs[MAXH * MAXW];
+
     fill(pairs, pairs + MAXH * MAXW, -1);
+    fill(hasPairs, hasPairs + MAXH * MAXW, false);
 
+    mcmf();
+
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+
+            if (CAMP[i][j] != '.' || (i + j) % 2)
+                continue;
+
+            for (int dir = 0; dir < 4; ++dir) {
+                int _i = i + dh[dir];
+                int _j = j + dw[dir];
+                int u =  i * W +  j;
+                int v = _i * W + _j;
+
+                if (safe(_i, _j) && FLOW[u + 2][v + 2] != 0) {
+                    pairs[u] = v;
+                    pairs[v] = u;
+                    hasPairs[u] = true;
+                    hasPairs[v] = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    list<int> part0;    ///< initialized with A.
+    list<int> part1;    ///< initialized with B - M.
+    list<int>::iterator it0, it1, target;
+    
     for (int i = 0; i < H; ++i) {
         for (int j = 0; j < W; ++j) {
 
             if (CAMP[i][j] != '.')
                 continue;
 
-            bool matched = false;
-            for (int dir = 0; dir < 4; ++dir) {
-                if (safe(i + dh[dir], j + dw[dir]) &&
-                    CAMP[i + dh[dir]][j + dw[dir]] == '.') {
-                    
-                    int u = i * W + j;
-                    int v = (i + dh[dir]) * W + j + dw[dir];
-
-                    if(FLOW[u + 2][v + 2] != 0) {
-                        pairs[u] = v;
-                        pairs[v] = u;
-                        matched = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!matched)
-                CAMP[i][j] = '^';
+            int v = i * W + j;
+            if ((i + j) % 2 == 0)
+                part0.push_back(v);
+            else if (!hasPairs[v])
+                part1.push_back(v);
         }
     }
+
+#ifndef CANUYES_DBG
+    cout << "A : ";
+    for (it0 = part0.begin(); it0 != part0.end(); ++it0)
+        cout << *it0 << " ";
+    cout << endl;
+    cout << "B - M : ";
+    for (it1 = part1.begin(); it1 != part1.end(); ++it1)
+        cout << *it1 << " ";
+    cout << endl;
+#endif
 
     while (true) {
 
-        int cnt = 0;
-        for (int i = 0; i < H; ++i) {
-            for (int j = 0; j < W; ++j) {
-
-                if (CAMP[i][j] != '.')
-                    continue;
-
-                int u = i * W + j;
-                int v = pairs[u];
-                
-                if (CAMP[u / W][u % W] != '.' ||
-                    CAMP[v / W][v % W] != '.')
-                    continue;
-
-                for (int dir = 0; dir < 4; ++dir) {
-                    if (safe(u / W + dh[dir], u % W + dw[dir]) &&
-                        CAMP[u / W + dh[dir]][u % W + dw[dir]] == '^') {
-
-                        CAMP[v / W][v % W] = '^';
-                        ++cnt;
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (cnt == 0) {
-
-            for (int i = 0; i < H; ++i) {
-                for (int j = 0; j < W; ++j) {
+        target = part0.end();
+        for (it0 = part0.begin(); it0 != part0.end(); ++it0) {
             
-                    if (CAMP[i][j] != '.')
-                        continue;
-            
-                    int u = i * W + j;
-                    int v = pairs[u];
-
-                    if (CAMP[u / W][u % W] == '.' &&
-                        CAMP[v / W][v % W] == '.') {
-
-                        CAMP[u / W][u % W] = '^';
-                        ++cnt;
-                        j = i = MAXH * MAXW;
-                    }
-                }
-            }
-
-            if (cnt == 0)
+            if (target != part0.end())
                 break;
+            
+            for (it1 = part1.begin(); it1 != part1.end(); ++it1) {
+
+                if (CAPA[(*it0) + 2][(*it1) + 2]) {
+                    target = it0;
+                    break;
+                }
+            }
         }
+
+        if (target == part0.end())
+            break;
+        
+        part1.push_back(pairs[*target]);
+        part0.erase(target);
     }
+
+    for (it0 = part0.begin(); it0 != part0.end(); ++it0)
+        CAMP[(*it0) / W][(*it0) % W] = '^';
+    for (it1 = part1.begin(); it1 != part1.end(); ++it1)
+        CAMP[(*it1) / W][(*it1) % W] = '^';
+    
+    return part0.size() + part1.size();
 }
 
 int main (void) {
@@ -168,28 +172,18 @@ int main (void) {
     cin >> tc;
     while (tc--) {
 
-        T = 0;
+        // initialize.
         memset(CAPA, 0, sizeof(int) * (MAXH * MAXW + 2) * (MAXH * MAXW + 2));
         memset(FLOW, 0, sizeof(int) * (MAXH * MAXW + 2) * (MAXH * MAXW + 2));
 
+        // input.
         cin >> H >> W;
         for (int i = 0; i < H; ++i) {
-            for (int j = 0; j < W; ++j) {
+            for (int j = 0; j < W; ++j)
                 cin >> CAMP[i][j];
-                if (CAMP[i][j] == '.')
-                    ++T;
-            }
         }
 
-#ifndef CANUYES_DBG
-        for (int i = 0; i < H; ++i) {
-            for (int j = 0; j < W; ++j) {
-                cout << CAMP[i][j];
-            }
-            cout << endl;
-        }
-#endif
-
+        // make graph.
         for (int i = 0; i < H; ++i) {
             for (int j = 0; j < W; ++j) {
 
@@ -197,25 +191,26 @@ int main (void) {
                     continue;
 
                 for (int dir = 0; dir < 4; ++dir) {
-                    if (safe(i + dh[dir], j + dw[dir]) &&
-                        CAMP[i + dh[dir]][j + dw[dir]] == '.') {
+                    int _i = i + dh[dir];
+                    int _j = j + dw[dir];
+
+                    if (safe(_i, _j) && CAMP[_i][_j] == '.') {
+                        int u =  i * W +  j;
+                        int v = _i * W + _j;
                         
-                        int u = i * W + j;
-                        int v = (i + dh[dir]) * W + j + dw[dir];
-                        
-                        CAPA[0][u + 2] = CAPA[v + 2][1] = CAPA[u + 2][v + 2] = 1;
+                        CAPA[0][u + 2] = 1;
+                        CAPA[v + 2][1] = 1;
+                        CAPA[u + 2][v + 2] = 1;
                     }
                 }
             }
         }
 
-        cout << T - mcmf() << endl;
-        
-        install();
+        // print answer.
+        cout << install() << endl;
         for (int i = 0; i < H; ++i) {
-            for (int j = 0; j < W; ++j) {
+            for (int j = 0; j < W; ++j)
                 cout << CAMP[i][j];
-            }
             cout << endl;
         }
     }
